21d685b533ec6de3facb38ae5526d568e5d0119e
Mofoks <31275278+SK000001@users.noreply.github.com>
2022-03-30 21:22:22 +0545

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,27 +1,21 @@
 def inisialiseP():
-    pArr = []
-
-    f = open("p.txt", "r+")
-    data = f.read(100)
-    f.seek(0)
-    if len(data) > 0:
-        for i in f:
-            x = i.replace("\n", "")
-            pArr.append(int(x))
-    else:
-        f.write("1\n1")
-        pArr = [1, 1]
-
-    f.close()
-
-    return pArr
+    try:
+        with open("p.txt", "a+") as f:
+            data = f.read()
+            if len(data) > 0:
+                return list(map(int, data.replace("\n", " ").split()))
+            else:
+                f.write("1\n1")
+                return [1, 1]
+    except:
+        with open("p.txt", "w") as f:
+            f.write("1\n1")
+        return [1, 1]
 
 def parition(pArr, x, y):
-    _x = x
-    _y = y
+    _x, _y, tempArr = x, y, pArr
     a = temp = sub = xDiff = 0
     b = count = yDiff = 1
-    tempArr = pArr
 
     while count > 0:
         if a == 4:
@@ -52,17 +46,16 @@ def main():
     n = int(input("enter n: "))
 
     if n+1 > len(p):
-        x = [1, 2, 3, 4, 5, 6, 7, 8,9,10]
-        y = [1, 3, 5, 7, 9, 11, 13, 15,17,19,21]
+        x, y = [i for i in range(1, 11)], [i for i in range(1, 22, 2)]
+
         while len(p) < n+1:
             if counter % 10 == 0:
                 x.append(x[-1]+1)
                 y.append(y[-1]+2)
 
             val = parition(p, x, y)
-            f = open("p.txt", "a")
-            f.write("\n"+str(val))
-            f.close()
+            with open("p.txt", "a") as f:
+                f.write("\n"+str(val))
             p.append(val)
 
     print("p(" + str(n) + ") = " + str(p[n]))
