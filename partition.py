8cfd66a6ddd77fbb9e3a87208ae5480a533ede73
Mofoks <31275278+SK000001@users.noreply.github.com>
2022-06-05 12:33:35 +0545

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,38 +1,46 @@
+import os
+
+os.system("clear")
+
 def inisialiseP():
-    try:
-        with open("p.txt", "a+") as f:
-            data = f.read()
-            if len(data) > 0:
-                return list(map(int, data.replace("\n", " ").split()))
-            else:
-                f.write("1\n1")
-                return [1, 1]
-    except:
-        with open("p.txt", "w") as f:
-            f.write("1\n1")
-        return [1, 1]
+    pArr = []
+
+    f = open("p.txt", "r+")
+    data = f.read(100)
+    f.seek(0)
+    if len(data) > 0:
+        for i in f:
+            x = i.replace("\n", "")
+            pArr.append(int(x))
+    else:
+        f.write("1\n1")
+        pArr = [1, 1]
+
+    f.close()
+
+    return pArr
+
 
 def parition(pArr, x, y):
-    _x, _y, tempArr = x, y, pArr
-    a = temp = sub = xDiff = 0
-    b = count = yDiff = 1
+    a, temp, sub, xDiff = 0, 0, 0, 0
+    b, count, yDiff = 1, 1
 
     while count > 0:
         if a == 4:
             a = -1
         else:
-            count = (len(tempArr)-1) - sub
+            count = (len(pArr)-1) - sub
             if count >= 0:
                 if a < 2:
-                    temp += tempArr[count]
+                    temp += pArr[count]
                 else:
-                    temp = temp - tempArr[count]
+                    temp = temp - pArr[count]
 
                 if b % 2 == 0:
-                    sub += _y[yDiff]
+                    sub += y[yDiff]
                     yDiff += 1
                 else:
-                    sub += _x[xDiff]
+                    sub += x[xDiff]
                     xDiff += 1
                 b += 1
         a += 1
@@ -46,16 +54,17 @@ def main():
     n = int(input("enter n: "))
 
     if n+1 > len(p):
-        x, y = [i for i in range(1, 11)], [i for i in range(1, 22, 2)]
-
+        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        y = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
         while len(p) < n+1:
             if counter % 10 == 0:
                 x.append(x[-1]+1)
                 y.append(y[-1]+2)
 
             val = parition(p, x, y)
-            with open("p.txt", "a") as f:
-                f.write("\n"+str(val))
+            f = open("p.txt", "a")
+            f.write("\n"+str(val))
+            f.close()
             p.append(val)
 
     print("p(" + str(n) + ") = " + str(p[n]))
