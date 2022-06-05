039b53825f4291d2d5c85189ff047f689de9eb57
Mofoks <31275278+SK000001@users.noreply.github.com>
2022-06-05 18:15:50 +0545

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,26 +1,35 @@
 def parition(pArr, x, y):
-    a, temp, sub, xDiff = 0, 0, 0, 0
+    a, temp, sub, xDiff = -1, 0, 0, 0
     b, count, yDiff = 1, 1, 1
 
     while count > 0:
+        a += 1
+        aflg, bflg = 0, 0
         if a == 4:
             a = -1
-        else:
-            count = (len(pArr)-1) - sub
-            if count >= 0:
-                if a < 2:
-                    temp += pArr[count]
-                else:
-                    temp = temp - pArr[count]
+            continue
 
-                if b % 2 == 0:
-                    sub += y[yDiff]
-                    yDiff += 1
-                else:
-                    sub += x[xDiff]
-                    xDiff += 1
-                b += 1
-        a += 1
+        count = (len(pArr)-1) - sub
+        if count < 0:
+            break
+
+        if a < 2:
+            temp += pArr[count]
+            aflg = 1
+        
+        if b % 2 == 0:
+            sub += y[yDiff]
+            yDiff += 1
+            bflg = 1
+
+        if aflg == 0:
+            temp -= pArr[count]
+
+        if bflg == 0:
+            sub += x[xDiff]
+            xDiff += 1
+        
+        b += 1
 
     return temp
 
@@ -33,7 +42,7 @@ def main():
     x = [1, 2]
     y = [1, 3]
     while len(p) < n+1:
-        if counter % 6 == 0:
+        if counter % 5 == 0:
             x.append(x[-1]+1)
             y.append(y[-1]+2)
 
